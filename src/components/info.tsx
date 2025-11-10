6bcd797fed0f59f1134f9c5a75d0f9868fcb4837
BADtochka <badmodertwitch@gmail.com>
2025-11-10 03:32:04 +0300

    fix: adaptive layout

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -60,10 +60,10 @@ export const Info: Component<InfoProps> = (props) => {
       animate={{ opacity: 1 }}
       class='mx-10 flex w-[70%] max-w-[800px] flex-col gap-12 rounded-2xl bg-[#191A18]/80 p-6'
     >
-      <div class='flex gap-4'>
+      <div class='flex gap-4 max-md:flex-col'>
         <Image src={data().icon ?? 'server.svg'} class='size-16 rounded-lg' />
         <div class='flex flex-col gap-2'>
-          <div class='flex items-center gap-4'>
+          <div class='flex flex-wrap items-center gap-4'>
             <h1 class='text-2xl font-bold'>{CONFIG.overrides?.name ?? data().motd.clean}</h1>
             <Show when={CONFIG.onlineChip || CONFIG.offlineChip}>
               <Status online={data().online} />
@@ -75,7 +75,7 @@ export const Info: Component<InfoProps> = (props) => {
         </div>
       </div>
       <div class='flex flex-wrap items-center justify-between gap-4 max-md:justify-center'>
-        <div class='flex gap-2'>
+        <div class='flex flex-wrap justify-center gap-2'>
           <Show when={data() && data().online}>
             <Chip icon='Users'>{`${CONFIG.overrides?.online ?? data().players.online} / ${data().players.max}`}</Chip>
           </Show>
@@ -86,7 +86,7 @@ export const Info: Component<InfoProps> = (props) => {
             </Chip>
           </Tooltip>
         </div>
-        <div class='flex gap-2'>
+        <div class='flex flex-wrap justify-center gap-2'>
           {CONFIG.links?.map((link) => (
             <a href={link.url} target='_blank'>
               <Chip icon={link.icon} class='cursor-pointer'>
